f6113a806156b283c6cab5c9a8e6529aebb163b3
optimusSAAD <saadbinomar998@gmail.com>
2019-12-21 16:05:21 +0600

    first commit

---
+++ resources/views/base.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MySQL CRUD</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="container">
+    @yield('main')
+    <div>
+        <a style="margin: 19px;" href="{{ route('contacts.create')}}" class="btn btn-primary">New contact</a>
+    </div>
+</div>
+<script src="{{ asset('js/app.js') }}" type="text/js"></script>
+</body>
+</html>
